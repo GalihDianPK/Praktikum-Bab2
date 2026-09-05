d21dca59196200a38d67db6e7dd65e05aa146b89
Kucing Capek <galihdpk0204@gmail.com>
2026-09-05 18:08:13 +0700

    Create Perceptron AND.py

---
+++ Percobaan 2.1/Perceptron AND.py
@@ -0,0 +1,20 @@
+import numpy as np
+# Import numpy untuk operasi array.
+
+w = np.load('weights.npy')
+b = np.load('bias.npy')
+# Memuat bobot dan bias hasil training.
+
+def step(x):
+    return 1 if x >= 0 else 0
+# Fungsi aktivasi step.
+
+X_test = np.array([[0,0],[0,1],[1,0],[1,1]])
+# Data uji untuk logic AND.
+
+print("Testing Perceptron Logic AND")
+for x in X_test:
+    z = np.dot(x, w) + b
+    y_pred = step(z)
+    print(f"Input: {x} -> Output Prediksi: {y_pred}")
+# Melakukan prediksi output untuk setiap input dan menampilkan hasil.
